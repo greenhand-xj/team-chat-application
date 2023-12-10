0da0285152e947a80b80725940b0b6f544843e5e
徐杰 <2275817182@qq.com>
2023-12-10 15:49:59 +0800

    feat: 创建channel功能done

---
+++ components/modals/create-channel-modal.tsx
@@ -0,0 +1,153 @@
+'use client'
+
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '../ui/dialog'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+import axios from 'axios'
+import { useParams, useRouter } from 'next/navigation'
+import { useModalStore } from '@/hooks/use-modal-store'
+import { ChannelType } from '@prisma/client'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../ui/select'
+import qs from 'query-string'
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(1, { message: 'Channel name is required.' })
+    .refine((name) => name !== 'general', {
+      message: 'Channel name cannot be "general".',
+    }),
+  type: z.nativeEnum(ChannelType),
+})
+
+export const CreateChannelModal = () => {
+  const router = useRouter()
+  const params = useParams()
+  const { isOpen, onClose, type } = useModalStore()
+  const isModalOpen = isOpen && type === 'createChannel'
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      type: ChannelType.TEXT,
+    },
+  })
+  const isLoading = form.formState.isSubmitting
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const url = qs.stringifyUrl({
+        url: '/api/channels',
+        query: {
+          serverId: params?.serverId,
+        },
+      })
+      await axios.post(url, values)
+      form.reset()
+      router.refresh()
+      onClose()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const handleClose = () => {
+    form.reset()
+    onClose()
+  }
+  return (
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Create Channel
+          </DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                      Channel name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                        disabled={isLoading}
+                        placeholder="Enter Channel name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="type"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Channel Type</FormLabel>
+                    <Select
+                      defaultValue={field.value}
+                      onValueChange={field.onChange}
+                      disabled={isLoading}
+                    >
+                      <FormControl>
+                        <SelectTrigger className="bg-zinc-300/50 border-0 focus:ring-0 text-black focus:ring-offset-0 ring-offset-0 outline-none capitalize">
+                          <SelectValue placeholder="Select a Channel Type" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {Object.values(ChannelType).map((type) => (
+                          <SelectItem
+                            key={type}
+                            value={type}
+                            className="capitalize"
+                          >
+                            {type.toLowerCase()}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button variant="primary" disabled={isLoading}>
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
+}
--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,6 +1,6 @@
 import { Server } from '@prisma/client'
 import { create } from 'zustand'
-export type ModalType = 'createServer' | 'invite' | 'editServer' | 'members'
+export type ModalType = 'createServer' | 'invite' | 'editServer' | 'members' | 'createChannel' | 'leaveServer'
 
 interface ModalData {
   server?: Server
