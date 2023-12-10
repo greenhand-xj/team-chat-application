716fab589eadc823fe09c2d0f6fbd3302eb1711d
徐杰 <2275817182@qq.com>
2023-12-10 16:26:59 +0800

    feat: 完成leave server的功能

---
+++ components/modals/leave-server-modal.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '../ui/dialog'
+
+import { useModalStore } from '@/hooks/use-modal-store'
+import { Button } from '../ui/button'
+import { useState } from 'react'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+export const LeaveServerModal = () => {
+  const { isOpen, onClose, type, data } = useModalStore()
+  const router = useRouter()
+  const isModalOpen = isOpen && type === 'leaveServer'
+  const { server } = data
+  const [isLoading, setIsLoading] = useState(false)
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true)
+      await axios.patch(`/api/servers/${server?.id}/leave`)
+      router.refresh()
+      router.push('/')
+      onClose()
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Leave Server
+          </DialogTitle>
+          <DialogDescription className='text-center text-zinc-500'>
+            Are you sure you want to leave <span className='font-semibold text-indigo-500'>{server?.name}</span>?
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className='bg-gray-100 px-6 py-4'>
+          <div className="flex items-center justify-between w-full">
+            <Button variant="ghost" onClick={onClose} disabled={isLoading}>
+              Cancel
+            </Button>
+            <Button onClick={onClick} disabled={isLoading} variant="primary">
+              Confirm
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
