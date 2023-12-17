97f2d1ff3411d5f489eb9c85b29f92283e012c02
徐杰 <2275817182@qq.com>
2023-12-17 16:21:26 +0800

    feat: 初始化socket.io

---
+++ pages/api/socket/io.ts
@@ -0,0 +1,24 @@
+import { NextApiResponseServerIO } from '@/types'
+import { Server as NetServer } from 'http'
+import { NextApiRequest } from 'next'
+import { Server as SocketIOServer } from 'socket.io'
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIO) => {
+  if (!res.socket.server.io) {
+    const path = '/api/socket/io'
+    const httpServer: NetServer = res.socket.server as any
+    const io = new SocketIOServer(httpServer, {
+      path,
+      addTrailingSlash: false
+    })
+    res.socket.server.io = io
+  }
+  res.end()
+}
+export default ioHandler
