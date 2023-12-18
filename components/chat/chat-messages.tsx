0dfa7f5c7024b5165fdd64e37022f5f933389bea
徐杰 <2275817182@qq.com>
2023-12-18 21:34:51 +0800

    feat: 使用react-query来获取message

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -2,6 +2,8 @@
 
 import { Member } from "@prisma/client"
 import { ChatWelcome } from "./chat-welcome"
+import { useChatQuery } from "@/hooks/use-chat-query"
+import { Loader2, ServerCrash } from "lucide-react"
 
 interface ChatMessagesProps {
   name: string
@@ -15,6 +17,33 @@ interface ChatMessagesProps {
   type: 'channel' | 'conversation'
 }
 export const ChatMessages = ({ name, member, chatId, apiUrl, socketUrl, socketQuery, paramKey, paramValue, type }: ChatMessagesProps) => {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } = useChatQuery({
+    queryKey: `chat:${chatId}`,
+    apiUrl,
+    paramKey,
+    paramValue,
+  })
+
+  if (status === 'pending') {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Loading messages...
+        </p>
+      </div>
+    )
+  }
+  if (status === 'error') {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Something went wrong!
+        </p>
+      </div>
+    )
+  }
   return (
     <div className="flex-1 flex flex-col py-4 overflow-y-auto">
       <div className="flex-1" />
